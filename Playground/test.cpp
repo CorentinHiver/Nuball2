d373f8a0627ab52d8af77a0573bbb8b7243823d8
Corentin Hiver <corentin.hiver@ijclab.in2p3.fr>
2024-02-16 15:44:58 +0100

    version  2.8.1_p6

--- Playground/test.cpp
+++ Playground/test.cpp
@@ -20,7 +20,7 @@
 int main(int argc, char ** argv)
 {
   detectors.load("index_129.list");
-  if (found(Path::pwd(), "faster")) MTObject::Initialize(10);
+  if (found(Path::pwd().string(), "faster")) MTObject::Initialize(10);
   else                              MTObject::Initialize(2);
   // Timeshifts ts;
   // ts.calculate("/home/corentin/faster_data/N-SI-136/60Co_center_after.fast", 20);
@@ -29,7 +29,7 @@ int main(int argc, char ** argv)
 
   CobaltCalorimeter cb;
   cb.loadCalibration(Calibration("../136/136_2024_Co.calib"));
-  if (found(Path::pwd(), "faster")) cb.launchRoot("~/nuball2/N-SI-136-sources/60Co_center_after");
+  if (found(Path::pwd().string(), "faster")) cb.launchRoot("~/nuball2/N-SI-136-sources/60Co_center_after");
   else                              cb.launchRoot("~/faster_data/N-SI-136-sources/60Co_center_after");
   // cb.loadTimeshifts(Timeshifts("Timeshifts/136_Co.dT"));
   // cb.loadTimeshifts(Timeshifts("/home/corentin/faster_data/N-SI-129-root/Timeshifts/run_156.dT"));
